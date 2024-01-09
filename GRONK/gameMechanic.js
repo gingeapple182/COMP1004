57aec0e00997dffc87c1899a85a154b326f51f58
Oliver <129097817+gingeapple182@users.noreply.github.com>
2024-01-09 01:54:03 +0000

    Separated state functions

---
+++ GRONK/gameMechanic.js
@@ -0,0 +1,8 @@
+function ratFight() {
+  background(0); //set background to black
+  fill(255); //set text colour to white
+  textSize(20);
+  text("You have encountered a wild rat", width/4, height/4);
+  text("Press 'f' to fight", width/4, height/4+30);
+  text("Press 'r' to run away", width/4, height/4+60);
+}
