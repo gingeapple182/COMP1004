941c9c6e9286c0b747d6d1db5aaab8ccb2b7394e
Oliver <129097817+gingeapple182@users.noreply.github.com>
2024-02-12 15:03:55 +0000

    Update gameMechanic.js
    Removed unneeded code

--- GRONK/gameMechanic.js
+++ GRONK/gameMechanic.js
@@ -58,40 +58,10 @@ function encounterMisc(misc, encounterTitle, encounterText, outcome) {
   textSize(20);
   drawText(encounterText, width/12, height/4, 250);
   drawText(outcome, width/12, height - height/3+30, 500);
-  /*text("Press 'r' to run away", width/2, height - height/3+60);
-  fill(255, 0, 0);
+  text('Press enter to continue', width/2, height - height/3+60);
+  /*fill(255, 0, 0);
   textSize(12);
   text("Lost " + hpLoss + "HP", width/2+200, height - height/3+60);*/
   currentScreen = 4;
   image(screens[currentScreen], 0,0, 700, 700);
-}
-
-function ratLose() {
-  console.log("ratLose function called");
-  currentScreen = 3;
-  image(screens[currentScreen], 0,0, 700, 700);
-  textAlign(LEFT);
-  fill(255); //set text colour to white
-  textSize(20);
-  text("You tried to fight without a weapon", width/4, height/4);
-  text("You lost", width/4, height/4+30);
-  text("Press 'r' to run away", width/4, height/4+60);
-  fill(255, 0, 0);
-  textSize(12);
-  text("Lost 20HP", width/4+200, height/4+30);
-}
-
-function ratBite() {
-  console.log("ratBite function called");
-  currentScreen = 3;
-  image(screens[currentScreen], 0,0, 700, 700);
-  textAlign(LEFT);
-  fill(255); //set text colour to white
-  textSize(20);
-  text("You tried running away", width/4, height/4);
-  text("The rat bit your ankle", width/4, height/4+30);
-  text("Press 'r' to run away", width/4, height/4+60);
-  fill(255, 0, 0);
-  textSize(12);
-  text("Lost 10HP", width/4+200, height/4+30);
 }
